34830520a67944e6ed7f8351955b7f47b89d08b7
Vaibhav Somani <vsomani2@gmail.com>
2025-07-20 13:42:40 -0500

    chore(deploy-failed): updated react table type issue

--- app/react-table.d.ts
+++ app/react-table.d.ts
@@ -2,5 +2,7 @@ import { CustomColumnMeta } from "@/components/data-table/types-and-interfaces";
 import "@tanstack/react-table";
 
 declare module "@tanstack/react-table" {
-  interface ColumnMeta extends CustomColumnMeta {}
+  interface ColumnMeta extends CustomColumnMeta {
+    __nonEmpty?: true;
+  }
 }
